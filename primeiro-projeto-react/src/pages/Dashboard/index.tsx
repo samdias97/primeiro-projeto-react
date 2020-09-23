2297fc36dbd737d6fb5a34c9ff15ce6689b1426a
samdias97 <samuel.sdias@hotmail.com>
2020-09-23 14:55:35 -0300

    Início

---
+++ primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="">
+          <img src="https://avatars3.githubusercontent.com/u/48606334?s=460&u=190def9f43f0bc3c9b5cce461e4b0dd7f421930e&v=4" 
+          alt="Samuel Dias" />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="">
+          <img src="https://avatars3.githubusercontent.com/u/48606334?s=460&u=190def9f43f0bc3c9b5cce461e4b0dd7f421930e&v=4" 
+          alt="Samuel Dias" />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="">
+          <img src="https://avatars3.githubusercontent.com/u/48606334?s=460&u=190def9f43f0bc3c9b5cce461e4b0dd7f421930e&v=4" 
+          alt="Samuel Dias" />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        
+      </Repositories>
+    </>
+  )
+}
+
+export default Dashboard;
